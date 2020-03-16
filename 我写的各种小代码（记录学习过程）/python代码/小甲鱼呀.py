0abebc354d2681fda17e1b94bdcbbefdf32e6786
1743294154 <1743294154@qq.com>
2020-03-16 15:25:59 +0800

    first m1

---
+++ 我写的各种小代码（记录学习过程）/python代码/小甲鱼呀.py
@@ -0,0 +1,10 @@
+print('李正制作')
+temp = input("不妨猜一下小甲鱼现在心里想的是那个数字：")
+guess = int(temp)
+if guess ==8:
+    print("我草，你是小甲鱼心里的蛔虫吗？")
+    print("哼，猜中了也没有奖励")
+else:
+    print("猜错了，小甲鱼现在心里想的是8！")
+print("游戏结束，不玩了")
+
---
+++ 我写的各种小代码（记录学习过程）/readme.txt
@@ -0,0 +1,3 @@
+Git is a version control system
+Git is free software
+这个软件夹下面的都是我的学习代码和上课的授课代码，但是很乱，只是记录学习过程而已。
